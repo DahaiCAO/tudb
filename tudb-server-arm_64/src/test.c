1e8c38a044106ec075b60442721d557a67cc09e2
cdh <cdh@cdh-VirtualBox>
2020-02-10 20:34:22 +0800

    tudb server development on ubuntu linux

---
+++ tudb-server-arm_64/src/test.c
@@ -0,0 +1,13 @@
+/*
+ * test.c
+ *
+ *  Created on: 2020年2月9日
+ *      Author: cdh
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argv, char ** argc) {
+	printf("%s\n", "hello world.");
+}
