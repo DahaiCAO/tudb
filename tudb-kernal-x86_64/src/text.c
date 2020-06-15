fb2257b2149655ad3ab2d9279a3db6f7ebd2074c
Dahai CAO <Dahai CAO@LAPTOP-2IM8KBP>
2020-06-15 21:24:46 +0800

    desgining the time axis storage.

--- tudb-kernal-x86_64/src/text.c
+++ tudb-kernal-x86_64/src/text.c
@@ -18,9 +18,8 @@
 
 // Convert long long to byte array
 void LongToByteArray(long long value, unsigned char buffer[]) {
-	memset(buffer, 0, sizeof buffer);
-	int BUFFER_SIZE = LONG_SIZE;
-	for (int i = 0; i < BUFFER_SIZE; i++) {
+	memset(buffer, 0, LONG_SIZE);
+	for (int i = 0; i < LONG_SIZE; i++) {
 		buffer[i] = ((value >> (LONG_SIZE * i)) & 0XFF);
 	}
 }
@@ -28,14 +27,50 @@ void LongToByteArray(long long value, unsigned char buffer[]) {
 // Convert byte array to long long
 long long ByteArrayToLong(unsigned char buffer[]) {
 	long long recoveredValue = 0;
-	int BUFFER_SIZE = LONG_SIZE;
-	for (int i = 0; i < BUFFER_SIZE; i++) {
-		//auto byteVal = ((buffer[i]) << (8 * i));
+	for (int i = 0; i < LONG_SIZE; i++) {
 		long long byteVal = (((long long) buffer[i]) << (LONG_SIZE * i));
-		//recoveredValue = recoveredValue + byteVal;
 		recoveredValue = recoveredValue | byteVal;
 	}
 	return recoveredValue;
+}
+
+void convertEpToBytes(evolved_point_t *ep, unsigned char buf[]) {
+	memset(buf, 0, 25);
+	buf[0] = ep->inUse;
+	unsigned char p[BUFFERSIZE] = { 0 };
+	unsigned char n[BUFFERSIZE] = { 0 };
+	unsigned char ts[BUFFERSIZE] = { 0 };
+	LongToByteArray(ep->prvTsId, p);
+	LongToByteArray(ep->nxtTsId, n);
+	LongToByteArray(ep->time, ts);
+	for (int i = 1; i <= 8; i++) {
+		buf[i] = p[i];
+	}
+	for (int i = 9; i <= 16; i++) {
+		buf[i] = n[i];
+	}
+	for (int i = 17; i <= 25; i++) {
+		buf[i] = ts[i];
+	}
+}
+
+void convertBytesToEp(evolved_point_t *ep, unsigned char buf[]) {
+	ep->inUse = buf[0];
+	unsigned char p[BUFFERSIZE] = { 0 };
+	unsigned char n[BUFFERSIZE] = { 0 };
+	unsigned char ts[BUFFERSIZE] = { 0 };
+	for (int i = 1; i <= 8; i++) {
+		p[i] = buf[i];
+	}
+	for (int i = 9; i <= 16; i++) {
+		n[i] = buf[i];
+	}
+	for (int i = 17; i <= 25; i++) {
+		ts[i] = buf[i];
+	}
+	ep->prvTsId = ByteArrayToLong(p);
+	ep->nxtTsId = ByteArrayToLong(n);
+	ep->time = ByteArrayToLong(ts);
 }
 
 /*
@@ -171,8 +206,8 @@ void readAllTaIds(FILE *idfp) {
 
 void createTimeAixs(long long ts, FILE *taidfp, FILE *tadbfp) {
 	// get a new Id
-	long long id = getId(taidfp);
-	unsigned char tsBytes[BUFFERSIZE] = { 0L };
+	unsigned char page[PAGESIZE] = { 0L };
+	unsigned char tsBytes[EPSIZE] = { 0L };
 	unsigned char p[BUFFERSIZE * 2] = { 0 };
 	unsigned char first[BUFFERSIZE] = { 0L };
 	unsigned char last[BUFFERSIZE] = { 0L };
@@ -188,9 +223,23 @@ void createTimeAixs(long long ts, FILE *taidfp, FILE *tadbfp) {
 	long long firstId = ByteArrayToLong(first);
 	long long lastId = ByteArrayToLong(last);
 	if (firstId == -1 && lastId == -1) {
-
+		// empty DB, insert it directly.
+		long long id = getId(taidfp); // get new Id
+		evolved_point_t *ep = (evolved_point_t*) malloc(sizeof(evolved_point_t));
+		ep->inUse = 1;
+		ep->nxtTsId = -1;
+		ep->prvTsId = -1;
+		ep->time = ts;//(unsigned long)time(NULL);
+		convertEpToBytes(ep, tsBytes);
+		fseek(tadbfp, 0L, SEEK_SET); // move file pointer to file head
+		unsigned char n1[BUFFERSIZE] = { 0L };
+		LongToByteArray(0, n1); // convert
+		fwrite(0, sizeof(unsigned char), LONG_SIZE, tadbfp);
+		fwrite(0, sizeof(unsigned char), LONG_SIZE, tadbfp);
+		fwrite(tsBytes, sizeof(unsigned char), EPSIZE, tadbfp);
+		free(ep);
 	} else {
-
+		// read one page
 	}
 
 }
--- tudb-kernal-x86_64/src/text.h
+++ tudb-kernal-x86_64/src/text.h
@@ -33,9 +33,44 @@
 #define TEXT_H_
 
 #define BUFFERSIZE 8L //
+#define PAGESIZE 2500L // read page size.
+#define EPSIZE 25L // evolved point size on time axis (25 bytes)
 const long LONG_SIZE = 8L; //
 const long TS_SIZE = 8L; //
+int TA_PAGES = 10; //
 FILE *taidfp = NULL;
 FILE *tadbfp = NULL;
+
+// the cache for new free and reused Ids
+typedef struct id_cache {
+	long long ids[1000];
+	long long reusedids[1000];
+} id_cache_t;
+
+typedef struct evolved_point {
+	unsigned char inUse; // if in use, 0: not in use, otherwise, 1: yes.
+	long long prvTsId; // previous evolved point(time stamp) Id
+	long long nxtTsId; // next evolved point(time stamp) Id
+	long long time; // time stamp
+} evolved_point_t;
+
+// time axis read buffer for update
+typedef struct ta_page {
+	long long expiretime; // expiration time to mark this page, -1 by default, that means blank page
+	unsigned char *content; // buffer for page content, 100 time axis records.
+	long long firstTsId; // first evolved point(time stamp) Id in this page
+	long long nextTsId; // next evolved point(time stamp) Id in this page
+	unsigned char duty; // if duty, then 1; otherwise, 0;
+	int hited; // hit counting, 0 by default, hit once, plus 1;
+} ta_page_t;
+
+typedef struct ta_book {
+	ta_page_t *pages; // 10 page by default.
+} ta_book_t;
+
+// memory page manager
+typedef struct tu_lib {
+	ta_book_t *tabook; //
+} tu_lib;
 
 #endif /* TEXT_H_ */
