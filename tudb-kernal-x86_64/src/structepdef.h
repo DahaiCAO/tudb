24323da0074a02a272e27d12e6002394177a2922
Dahai CAO <Dahai CAO@LAPTOP-2IM8KBP>
2020-06-24 21:15:45 +0800

    designing time axis store

--- tudb-kernal-x86_64/src/structepdef.h
+++ tudb-kernal-x86_64/src/structepdef.h
@@ -25,9 +25,11 @@
 #define STRUCTEPDEF_H_
 
 typedef struct evolved_point {
+	long long id; //
 	long long prvTsId; // previous evolved point(time stamp) Id
 	long long nxtTsId; // next evolved point(time stamp) Id
 	long long time; // time stamp
+	unsigned char *pos;
 } evolved_point_t;
 
 // time axis read buffer for update
@@ -36,36 +38,40 @@ typedef struct ta_page {
 	unsigned char *content; // buffer for page content, 100 time axis records.
 	long long start; // start point on this page
 	long long end; // end point on this page
-	unsigned char duty; // if duty, then 1; otherwise, 0;
+	unsigned char dirty; // if dirty, then 1; otherwise, 0;
 	int hited; // hit counting, 0 by default, hit once, plus 1;
-	struct ta_page * prvpage;
-	struct ta_page * nxtpage;
+	struct ta_page *prvpage;
+	struct ta_page *nxtpage;
 } ta_page_t;
 
 typedef struct ta_buf {
-	long long first;// address of the first time axis record
+	long long first; // address of the first time axis record
 	long long last; // address of the second time axis record
 	ta_page_t *pages; // 10 page by default.
 } ta_buf_t;
 
-ta_buf_t * timeaxispages;
+ta_buf_t *timeaxispages;
+
+typedef struct ep_queue_node {
+	evolved_point_t *ep;
+	struct ep_queue_node *next;
+} ep_queue_node_t;
+
+ep_queue_node_t *queue;
 
 /*
 
 
-typedef struct ta_book {
-	ta_page_t *pages; // 10 page by default.
-} ta_book_t;
+ typedef struct ta_book {
+ ta_page_t *pages; // 10 page by default.
+ } ta_book_t;
 
-// memory page manager
-typedef struct tu_lib {
-	ta_book_t *tabook; //
-} tu_lib;
+ // memory page manager
+ typedef struct tu_lib {
+ ta_book_t *tabook; //
+ } tu_lib;
 
-*/
+ */
 
 #endif /* STRUCTEPDEF_H_ */
-
-
-
 
--- tudb-kernal-x86_64/src/tastore.c
+++ tudb-kernal-x86_64/src/tastore.c
@@ -22,44 +22,79 @@
  * Author: Dahai CAO
  */
 
-void convertEpToBytes(evolved_point_t *ep, unsigned char buf[]) {
-	memset(buf, 0, 25);
+//void convertEpToBytes(evolved_point_t *ep, unsigned char buf[]) {
+//	memset(buf, 0, 25);
+//	unsigned char prv[LONG_LONG] = { 0 };
+//	unsigned char nxt[LONG_LONG] = { 0 };
+//	unsigned char ts[LONG_LONG] = { 0 };
+//	LongToByteArray(ep->prvTsId, prv);
+//	LongToByteArray(ep->nxtTsId, nxt);
+//	LongToByteArray(ep->time, ts);
+//	for (int i = 0; i < 8; i++) {
+//		buf[i] = prv[i];
+//	}
+//	for (int i = 8; i < 16; i++) {
+//		buf[i] = nxt[i];
+//	}
+//	for (int i = 16; i < 24; i++) {
+//		buf[i] = ts[i];
+//	}
+//}
+//
+//void convertBytesToEp(evolved_point_t *ep, unsigned char buf[]) {
+//	unsigned char prv[LONG_LONG] = { 0 };
+//	unsigned char nxt[LONG_LONG] = { 0 };
+//	unsigned char ts[LONG_LONG] = { 0 };
+//	for (int i = 0; i < 8; i++) {
+//		prv[i] = buf[i];
+//	}
+//	for (int i = 8; i < 16; i++) {
+//		nxt[i] = buf[i];
+//	}
+//	for (int i = 16; i < 24; i++) {
+//		ts[i] = buf[i];
+//	}
+//	ep->prvTsId = ByteArrayToLong(prv);
+//	ep->nxtTsId = ByteArrayToLong(nxt);
+//	ep->time = ByteArrayToLong(ts);
+//}
+void getEvolvedPoint(evolved_point_t *ep, unsigned char *p) {
+	unsigned char prv[LONG_LONG] = { 0L };
+	unsigned char nxt[LONG_LONG] = { 0L };
+	unsigned char tms[LONG_LONG] = { 0L };
+	for (int i = 0; i < LONG_LONG; i++) {
+		prv[i] = *(p + i);
+	}
+	for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
+		nxt[i] = *(p + i);
+	}
+	for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
+		tms[i] = *(p + i);
+	}
+	ep->prvTsId = ByteArrayToLong(prv);
+	ep->nxtTsId = ByteArrayToLong(nxt);
+	ep->time = ByteArrayToLong(tms);
+}
+
+void putEvolvedPoint(evolved_point_t *ep) {
 	unsigned char prv[LONG_LONG] = { 0 };
 	unsigned char nxt[LONG_LONG] = { 0 };
 	unsigned char ts[LONG_LONG] = { 0 };
 	LongToByteArray(ep->prvTsId, prv);
 	LongToByteArray(ep->nxtTsId, nxt);
 	LongToByteArray(ep->time, ts);
-	for (int i = 0; i < 8; i++) {
-		buf[i] = prv[i];
+	for (int i = 0; i < LONG_LONG; i++) {
+		*(ep->pos + i) = prv[i];
 	}
-	for (int i = 8; i < 16; i++) {
-		buf[i] = nxt[i];
+	for (int i = 0; i < LONG_LONG; i++) {
+		*(ep->pos + i + LONG_LONG) = nxt[i];
 	}
-	for (int i = 16; i < 24; i++) {
-		buf[i] = ts[i];
+	for (int i = 0; i < LONG_LONG; i++) {
+		*(ep->pos + i + 2 * LONG_LONG) = ts[i];
 	}
 }
 
-void convertBytesToEp(evolved_point_t *ep, unsigned char buf[]) {
-	unsigned char prv[LONG_LONG] = { 0 };
-	unsigned char nxt[LONG_LONG] = { 0 };
-	unsigned char ts[LONG_LONG] = { 0 };
-	for (int i = 0; i < 8; i++) {
-		prv[i] = buf[i];
-	}
-	for (int i = 8; i < 16; i++) {
-		nxt[i] = buf[i];
-	}
-	for (int i = 16; i < 24; i++) {
-		ts[i] = buf[i];
-	}
-	ep->prvTsId = ByteArrayToLong(prv);
-	ep->nxtTsId = ByteArrayToLong(nxt);
-	ep->time = ByteArrayToLong(ts);
-}
-
-ta_page_t* readOnePage(long long start, FILE *tadbfp) {
+void readOnePage(long long start, FILE *tadbfp) {
 	unsigned char page[TA_PAGE_SIZE] = { 0L };
 	fseek(tadbfp, start, SEEK_SET); // start to read from the first record
 	int c;
@@ -69,7 +104,7 @@ ta_page_t* readOnePage(long long start, FILE *tadbfp) {
 	}
 
 	ta_page_t *p = (ta_page_t*) malloc(sizeof(ta_page_t));
-	p->duty = 0;
+	p->dirty = 0;
 	p->expiretime = 10; // 10 minutes
 	p->hited = 0;
 	p->nxtpage = NULL;
@@ -86,42 +121,103 @@ ta_page_t* readOnePage(long long start, FILE *tadbfp) {
 		pp->nxtpage = p;
 		p->prvpage = pp;
 	} else {
-		pp = p;
+		timeaxispages->pages = p;
 	}
-	return pp;
 }
 
-void saveEvolvedPoint(long long ts, FILE *taidfp, FILE *tadbfp) {
-//	unsigned char first[LONG_LONG] = { 0L };
-//	unsigned char last[LONG_LONG] = { 0L };
-//	unsigned char p[LONG_LONG * 2] = { 0 };
-//	fseek(tadbfp, 0L, SEEK_SET); //
-//	fread(p, sizeof(unsigned char), LONG_LONG * 2, tadbfp);
-//	for (int i = 0; i < LONG_LONG; i++) {
-//		first[i] = p[i];
-//	}
-//	for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
-//		last[i] = p[i];
-//	}
-//	long long firstId = ByteArrayToLong(first);
-//	long long lastId = ByteArrayToLong(last);
+void lookupInPage(long long ts, unsigned char *position, long long start,
+		long long end, evolved_point_t *tmp, FILE *tadbfp) {
+	unsigned char *p = position;
+	getEvolvedPoint(tmp, p);
+	if (tmp->time == 0) {
+		// this is empty DB, so ts will be stored here
+		tmp->pos = position;
+	} else if (tmp->time != 0) {
+		if (ts > tmp->time) {
+			// lookup forwards
+			long long n = start + tmp->nxtTsId * (3 * LONG_LONG);
+			if (n > end) {
+				// look up other pages, if not exists, read one page again;
+				ta_page_t *pp = timeaxispages->pages;
+				if (pp != NULL) {
+					int r = -1;
+					while (pp != NULL) {
+						if (n > pp->start && n < pp->end) {
+							evolved_point_t *ep = (evolved_point_t*) malloc(
+									sizeof(evolved_point_t));
+							lookupInPage(ts, pp->content, pp->start, pp->end,
+									ep, tadbfp);
+							if (r == 1) {
+								break; // found
+							}
+						} else {
+							if (pp->nxtpage != NULL) {
+								pp = pp->nxtpage;
+							} else {
+								readOnePage(pp->end, tadbfp);
+							}
+						}
+					}
+					if (r == -1) {
+						//
+					}
+				}
+			} else {
 
-//	for (int i = 0; i < 10; i++) {
-//		unsigned char p[LONG_LONG] = { 0L };
-//		unsigned char n[LONG_LONG] = { 0L };
-//		unsigned char t[LONG_LONG] = { 0L };
-//		int start = 24 * i;
-//		for (int j = start; j < start + 8; j++) {
-//			p[j] = page[j];
-//		}
-//		for (int j = start + 8; j < start + 16; j++) {
-//			n[j] = page[j];
-//		}
-//		for (int j = start + 16; j < start + 24; j++) {
-//			t[j] = page[j];
-//		}
-//
-//	}
+			}
+		} else if (ts < tmp->time) {
+			// lookup backwards
+			long long p = tmp->prvTsId + 3 * LONG_LONG;
+			// look up other pages, if not exists, read one page again;
+			if (p < start) {
+
+			}
+		} else {
+
+		}
+	}
+}
+
+void commitEvolvedPoint(evolved_point_t *ep, FILE *tadbfp) {
+
+}
+
+void updateEvolvedPoint(long long ts, FILE *taidfp, FILE *tadbfp) {
+	ta_page_t *pp = timeaxispages->pages;
+	if (pp != NULL) {
+		evolved_point_t *ep = (evolved_point_t*) malloc(
+				sizeof(evolved_point_t));
+		while (pp != NULL) {
+			if (pp->expiretime != -1) { // does not expired
+				lookupInPage(ts, pp->content, pp->start, pp->end, ep, tadbfp);
+			}
+			if (ep->pos != NULL) { // found a position
+				break;
+			} else {
+				pp = pp->nxtpage;
+			}
+		}
+		if (ep->pos != NULL) { // found a position
+			// insert the evolved point at the position
+			evolved_point_t *nwep = (evolved_point_t*) malloc(
+					sizeof(evolved_point_t));
+			long long id = getOneId();
+			nwep->id = id;
+			nwep->nxtTsId = ep->nxtTsId;
+			nwep->prvTsId = ep->prvTsId;
+			nwep->time = ts;
+			nwep->pos = ep->pos;
+
+			putEvolvedPoint(nwep);
+			pp->dirty = 1;
+			free(nwep);
+			//for (int i = 0; i < 24; i++) {
+			//	printf("%d\n", *(r + i));
+			//}
+			free(ep);
+		}
+	}
+
 }
 
 /*
@@ -167,8 +263,9 @@ void saveEvolvedPoint(long long ts, FILE *taidfp, FILE *tadbfp) {
  }*/
 
 int main(int argv, char **argc) {
+
 	setvbuf(stdout, NULL, _IONBF, 0);
-	// get new Id from next free IDs
+// get new Id from next free IDs
 	char *taid = "D:/tudata/tustore.timeaxis.tdb.id";
 	FILE *taidfp = fopen(taid, "rb+");
 	char *tadb = "D:/tudata/tustore.timeaxis.tdb";
@@ -186,20 +283,37 @@ int main(int argv, char **argc) {
 	timeaxispages = (ta_buf_t*) malloc(sizeof(ta_buf_t));
 	timeaxispages->pages = NULL;
 
+	if (timeaxispages->pages == NULL) {
+		readOnePage(16L, tadbfp);
+	}
+
 	//loadIds(taidfp);
 	//readAllTaIds(taidfp);
 
-	ta_page_t *p = readOnePage(16L, tadbfp);
-
 	long long ts = (unsigned long) time(NULL);
-	saveEvolvedPoint(ts, taidfp, tadbfp);
+	updateEvolvedPoint(ts, taidfp, tadbfp);
+	long long ts1 = (unsigned long) time(NULL);
+	updateEvolvedPoint(ts1, taidfp, tadbfp);
+
+	// commitEvolvedPoint(ep, tadbfp);
 
 	fclose(taidfp);
 	fclose(tadbfp);
 
-//	free(timeaxispages);
-//	free(cache);
-//	printf("%lld\d", id);
-
+	free(timeaxispages);
+	free(cache);
+	//	printf("%lld\d", id);
+	/*
+	 unsigned char *p = (unsigned char*) malloc(sizeof(unsigned char) * 3);
+	 unsigned char a[3] = { 1, 2, 3 };
+	 for (int i = 0; i < 3; i++) {
+	 *(p + i) = a[i];
+	 }
+	 //
+	 for (int i = 0; i < 3; i++) {
+	 printf("%d\n",*(p+i));
+	 //printf("%d\n",a[i]);
+	 }
+	 */
 }
 
