45ebc0ed9cd8807a19a80ca60fb497d9a4ff8c75
Dahai CAO <Dahai CAO@LAPTOP-2IM8KBP>
2020-06-27 22:28:40 +0800

    designing time axis store

--- tudb-kernal-x86_64/src/tastore.c
+++ tudb-kernal-x86_64/src/tastore.c
@@ -59,41 +59,41 @@
 //	ep->time = ByteArrayToLong(ts);
 //}
 /*void getEvolvedPoint(evolved_point_t *ep, unsigned char *p) {
-	unsigned char prv[LONG_LONG] = { 0L };
-	unsigned char nxt[LONG_LONG] = { 0L };
-	unsigned char tms[LONG_LONG] = { 0L };
-	for (int i = 0; i < LONG_LONG; i++) {
-		prv[i] = *(p + i);
-	}
-	for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
-		nxt[i] = *(p + i);
-	}
-	for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
-		tms[i] = *(p + i);
-	}
-	ep->prvTsId = ByteArrayToLong(prv);
-	ep->nxtTsId = ByteArrayToLong(nxt);
-	ep->time = ByteArrayToLong(tms);
-}
+ unsigned char prv[LONG_LONG] = { 0L };
+ unsigned char nxt[LONG_LONG] = { 0L };
+ unsigned char tms[LONG_LONG] = { 0L };
+ for (int i = 0; i < LONG_LONG; i++) {
+ prv[i] = *(p + i);
+ }
+ for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
+ nxt[i] = *(p + i);
+ }
+ for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
+ tms[i] = *(p + i);
+ }
+ ep->prvTsId = ByteArrayToLong(prv);
+ ep->nxtTsId = ByteArrayToLong(nxt);
+ ep->time = ByteArrayToLong(tms);
+ }
 
-void parseEvolvedPoint(long long prvId, long long nxtId, long long ts,
-		unsigned char *p) {
-	unsigned char prv[LONG_LONG] = { 0L };
-	unsigned char nxt[LONG_LONG] = { 0L };
-	unsigned char tms[LONG_LONG] = { 0L };
-	for (int i = 0; i < LONG_LONG; i++) {
-		prv[i] = *(p + i);
-	}
-	for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
-		nxt[i] = *(p + i);
-	}
-	for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
-		tms[i] = *(p + i);
-	}
-	prvId = ByteArrayToLong(prv);
-	nxtId = ByteArrayToLong(nxt);
-	ts = ByteArrayToLong(tms);
-}*/
+ void parseEvolvedPoint(long long prvId, long long nxtId, long long ts,
+ unsigned char *p) {
+ unsigned char prv[LONG_LONG] = { 0L };
+ unsigned char nxt[LONG_LONG] = { 0L };
+ unsigned char tms[LONG_LONG] = { 0L };
+ for (int i = 0; i < LONG_LONG; i++) {
+ prv[i] = *(p + i);
+ }
+ for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
+ nxt[i] = *(p + i);
+ }
+ for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
+ tms[i] = *(p + i);
+ }
+ prvId = ByteArrayToLong(prv);
+ nxtId = ByteArrayToLong(nxt);
+ ts = ByteArrayToLong(tms);
+ }*/
 
 long long parsePrvId(unsigned char *p) {
 	unsigned char prv[LONG_LONG] = { 0L };
@@ -105,16 +105,16 @@ long long parsePrvId(unsigned char *p) {
 
 long long parseNxtId(unsigned char *p) {
 	unsigned char nxt[LONG_LONG] = { 0L };
-	for (int i = LONG_LONG; i < 2 * LONG_LONG; i++) {
-		nxt[i] = *(p + i);
+	for (int i = 0; i < LONG_LONG; i++) {
+		nxt[i] = *(p + i + LONG_LONG);
 	}
 	return ByteArrayToLong(nxt);
 }
 
 long long parseStamp(unsigned char *p) {
 	unsigned char tms[LONG_LONG] = { 0L };
-	for (int i = 2 * LONG_LONG; i < 3 * LONG_LONG; i++) {
-		tms[i] = *(p + i);
+	for (int i = 0; i < LONG_LONG; i++) {
+		tms[i] = *(p + i + 2 * LONG_LONG);
 	}
 	return ByteArrayToLong(tms);
 }
@@ -190,18 +190,35 @@ ta_page_t* findPage(long long id) {
  * ts
  * tmp: search result
  */
-void lookupInPage(long long ts, unsigned char *curr, unsigned char *pstart,
-		long long startNo, long long start, long long end, evolved_point_t *tmp,
+void lookup(long long ts, unsigned char *curr, ta_page_t *page,
+		evolved_point_t *tmp, evolved_point_t *previous, evolved_point_t *next,
 		FILE *tadbfp) {
+	// current record, it may be previous/next record
 	long long prvId = parsePrvId(curr);
 	long long nxtId = parseNxtId(curr);
 	long long stamp = parseStamp(curr);
 	if (stamp == 0) {
-		// this is empty DB, so ts will be stored here
+		// this is an empty DB,
+		// so this time stamp will be stored here
 		tmp->pos = curr;
+		tmp->nxtTsId = NULL_POINTER;
+		tmp->prvTsId = NULL_POINTER;
 		return;
 	} else if (stamp != 0) {
-		if (ts > stamp) {
+		if (ts < stamp) { // current record will be next record
+			if (prvId == NULL_POINTER) {
+				long long id = getOneId();
+				tmp->id = id;//
+				tmp->prvTsId = NULL_POINTER;
+				tmp->nxtTsId = timeaxispages->first;
+				next->prvTsId = id;//
+				next->pos = curr;
+				next->nxtTsId = nxtId;
+				timeaxispages->first = id;
+			} else {
+
+			}
+		} else if (ts > stamp) { // current record will be previous record
 			// found current record Id
 			if (prvId != NULL_POINTER) {
 				ta_page_t *prp = findPage(prvId);
@@ -215,63 +232,80 @@ void lookupInPage(long long ts, unsigned char *curr, unsigned char *pstart,
 				long long nxtId1 = parseNxtId(p1);
 				tmp->prvTsId = nxtId1;
 			} else {
-				tmp->prvTsId = NULL_POINTER;
+				// the current is the first record
+				previous->pos = curr;
+				previous->nxtTsId = 0; // not assign a Id;
+				previous->prvTsId = prvId;
+				tmp->prvTsId = 0;
+
 			}
 			// lookup forwards
-			long long nxt = start + nxtId * (3 * LONG_LONG);
-			if (nxt <= end) { // in this page
-				unsigned char *p2 = pstart
-						+ (nxtId - startNo) * (3 * LONG_LONG);
-				lookupInPage(ts, p2, pstart, startNo, start, end, tmp, tadbfp);
-			} else if (nxt > end) { // not in this page
+			if (nxtId != -2) {
+				long long nxt = page->start + nxtId * (3 * LONG_LONG);
+				if (nxt <= page->end) { // in this page
+					unsigned char *p2 = page->content
+							+ (nxtId - page->startNo) * (3 * LONG_LONG);
+					lookup(ts, p2, page, tmp, previous, next, tadbfp);
+				} else if (nxt > page->end) { // not in this page
+					ta_page_t *nxp = findPage(nxtId);
+					if (nxp == NULL) {
+						long long pn = (nxtId * (3 * LONG_LONG)) / 240;
+						long long st = pn * 240 + 16L;
+						nxp = readOnePage(st, pn * 10, tadbfp);
+					}
+					unsigned char *p3 = nxp->content
+							+ (nxtId - nxp->startNo) * (3 * LONG_LONG);
+					lookup(ts, p3, nxp, tmp, previous, next, tadbfp);
+				}
+			} else {
+
+				tmp->nxtTsId = -2;
+			}
+		} else if (ts < stamp) { // current record will be next record
+			if (nxtId != NULL_POINTER) {
 				ta_page_t *nxp = findPage(nxtId);
 				if (nxp == NULL) {
 					long long pn = (nxtId * (3 * LONG_LONG)) / 240;
 					long long st = pn * 240 + 16L;
 					nxp = readOnePage(st, pn * 10, tadbfp);
 				}
-				unsigned char *p3 = nxp->content
+				unsigned char *p1 = nxp->content
 						+ (nxtId - nxp->startNo) * (3 * LONG_LONG);
-				lookupInPage(ts, p3, nxp->content, nxp->startNo, nxp->start,
-						nxp->end, tmp, tadbfp);
+				long long prvId1 = parsePrvId(p1);
+				tmp->nxtTsId = prvId1;
+			} else {
+				tmp->nxtTsId = NULL_POINTER;
+			}
+			if (prvId != -2) {
+				// lookup backwards
+				long long prv = page->start + prvId * (3 * LONG_LONG);
+				if (prv >= page->start) { // in this page
+					unsigned char *p2 = page->content
+							+ (prvId - page->startNo) * (3 * LONG_LONG);
+					lookup(ts, p2, page, tmp, previous, next, tadbfp);
+				} else if (prv < page->start) {
+					ta_page_t *prp = findPage(prvId);
+					if (prp == NULL) {
+						long long pn = (prvId * (3 * LONG_LONG)) / 240;
+						long long st = pn * 240 + 16L;
+						prp = readOnePage(st, pn * 10, tadbfp);
+					}
+					unsigned char *p3 = prp->content
+							+ (prvId - prp->startNo) * (3 * LONG_LONG);
+					lookup(ts, p3, prp, tmp, previous, next, tadbfp);
+				}
+			} else {
+
 			}
 		}
-	} else if (ts < stamp) {
-		if (nxtId != NULL_POINTER) {
-			ta_page_t *nxp = findPage(nxtId);
-			if (nxp == NULL) {
-				long long pn = (nxtId * (3 * LONG_LONG)) / 240;
-				long long st = pn * 240 + 16L;
-				nxp = readOnePage(st, pn * 10, tadbfp);
-			}
-			unsigned char *p1 = nxp->content
-					+ (nxtId - nxp->startNo) * (3 * LONG_LONG);
-			long long prvId1 = parsePrvId(p1);
-			tmp->nxtTsId = prvId1;
-		} else {
-			tmp->nxtTsId = NULL_POINTER;
-		}
-		// lookup backwards
-		long long prv = start + prvId * (3 * LONG_LONG);
-		if (prv >= start) { // in this page
-			unsigned char *p2 = pstart + (prvId - startNo) * (3 * LONG_LONG);
-			lookupInPage(ts, p2, pstart, startNo, start, end, tmp, tadbfp);
-		} else if (prv < start) {
-			ta_page_t *prp = findPage(prvId);
-			if (prp == NULL) {
-				long long pn = (prvId * (3 * LONG_LONG)) / 240;
-				long long st = pn * 240 + 16L;
-				prp = readOnePage(st, pn * 10, tadbfp);
-			}
-			unsigned char *p3 = prp->content
-					+ (prvId - prp->startNo) * (3 * LONG_LONG);
-			lookupInPage(ts, p3, prp->content, prp->startNo, prp->start,
-					prp->end, tmp, tadbfp);
-		}
+	} else if (ts == stamp) {
+		tmp->time = ts;
+		return; // not handle
 	}
-	if (tmp->nxtTsId != -3 && tmp->nxtTsId != -3) {
+	if (tmp->nxtTsId != -3L && tmp->prvTsId != -3L) {
 		return;
 	}
+
 }
 
 void commitEvolvedPoint(evolved_point_t *ep, FILE *tadbfp) {
@@ -284,11 +318,21 @@ void updateEvolvedPoint(long long ts, FILE *taidfp, FILE *tadbfp) {
 		evolved_point_t *t = (evolved_point_t*) malloc(sizeof(evolved_point_t));
 		t->prvTsId = -3;
 		t->nxtTsId = -3;
-		t->time = ts;
+		t->time = 0;
+		t->id = -3;
+		evolved_point_t *p = (evolved_point_t*) malloc(sizeof(evolved_point_t));
+		p->prvTsId = -3;
+		p->nxtTsId = -3;
+		p->time = 0;
+		p->id = -3;
+		evolved_point_t *n = (evolved_point_t*) malloc(sizeof(evolved_point_t));
+		n->prvTsId = -3;
+		n->nxtTsId = -3;
+		n->time = 0;
+		n->id = -3;
 		while (pp != NULL) {
 			if (pp->expiretime != -1) { // does not expired
-				lookupInPage(ts, pp->content, pp->content, pp->startNo,
-						pp->start, pp->end, t, tadbfp);
+				lookup(ts, pp->content, pp, t, p, n, tadbfp);
 			}
 			if (t->pos != NULL) { // found a position
 				break;
@@ -298,23 +342,19 @@ void updateEvolvedPoint(long long ts, FILE *taidfp, FILE *tadbfp) {
 		}
 		if (t->pos != NULL && t->time != ts) { // found a position
 		// insert the evolved point at the position
-			evolved_point_t *nwep = (evolved_point_t*) malloc(
-					sizeof(evolved_point_t));
 			long long id = getOneId();
-			nwep->id = id;
-			nwep->nxtTsId = t->nxtTsId;
-			nwep->prvTsId = t->prvTsId;
-			nwep->time = ts;
-			nwep->pos = t->pos;
-
-			putEvolvedPoint(nwep);
+			t->id = id;
+			t->time = ts;
+			putEvolvedPoint(t);
 			pp->dirty = 1;
-			free(nwep);
-			//for (int i = 0; i < 24; i++) {
-			//	printf("%d\n", *(r + i));
-			//}
+			// update previous evolved point
+
+			// update next evolved point
+
 		}
 		free(t);
+		free(p);
+		free(n);
 	}
 
 }
